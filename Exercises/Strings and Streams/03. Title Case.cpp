12e25ea28b0e768c7b210a8281d43867a0dde329
Daniel Pramatarov <63365611+DanielPramatarov@users.noreply.github.com>
2020-06-22 22:39:53 +0300

    Create  03. Title Case.cpp

---
+++ Exercises/Strings and Streams/03. Title Case.cpp
@@ -0,0 +1,33 @@
+
+
+    #include <iostream>
+    #include <string>
+    using namespace std;
+     
+    int main() {        
+        bool in_skip_state {true};  
+        string sentence;
+        string result;
+            
+        getline(cin, sentence);
+
+        for (int c = 0; c < sentence.size(); ++c)
+        {
+            if (in_skip_state) {
+                if (isalpha(sentence.at(c))) {
+                    in_skip_state = false;
+                    sentence.at(c) = toupper(sentence.at(c));
+                }
+            } else {
+                 if (!isalpha(sentence.at(c)))
+                 {
+                    in_skip_state = true;
+                 }     
+            } 
+            result += sentence.at(c);
+        }
+
+       // cout << "Sentence: " << sentence << endl;
+        cout  << result << endl;
+        return 0; 
+    }
