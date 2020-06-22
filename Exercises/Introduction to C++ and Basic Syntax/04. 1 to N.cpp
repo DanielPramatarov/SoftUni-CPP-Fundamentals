d01dbee9c7e90d787260be5adbd318a16cb716e9
Daniel Pramatarov <63365611+DanielPramatarov@users.noreply.github.com>
2020-06-22 21:45:38 +0300

    Create 04. 1 to N.cpp

---
+++ Exercises/Introduction to C++ and Basic Syntax/04. 1 to N.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+int main(){
+
+    int number;
+    cin >> number;
+
+    for(int i = 1; i <= number; i++){
+
+        cout << i << " ";
+    }
+
+
+
+
+
+    return 0;
+}
+
