b417dfa9a23f6ec79ff18aa2c505d1fa03bc92b8
Daniel Pramatarov <63365611+DanielPramatarov@users.noreply.github.com>
2020-06-22 22:45:49 +0300

    Create  01. Compare Matrices.cpp

---
+++ Exercises/STL Linear Containers/01. Compare Matrices.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+int main(){
+  int rows;
+  cin >> rows;
+  cin.ignore();
+  
+  vector<string> V(rows), V2(rows);
+  
+  for(int i = 0; i < rows; i++)
+    getline(cin,V[i]);
+ 
+ cin >> rows;
+ cin.ignore();
+  
+  if(rows==V.size())
+    for(int i = 0; i < rows; i++)
+      getline(cin,V2[i]);
+ 
+ cout << (V == V2? "equal":"not equal") << endl;
+}
